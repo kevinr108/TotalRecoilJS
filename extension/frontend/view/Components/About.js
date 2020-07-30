79050c8f35356657b6016b6f981bcbd81968de9f
Kevin <ruan8111@gmail.com>
2020-07-29 23:48:42 -0400

    clean up somethings

--- extension/frontend/view/Components/About.js
+++ extension/frontend/view/Components/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Tester
 const About = () => {
   return (
     <div>About</div>
--- extension/frontend/view/view.js
+++ extension/frontend/view/view.js
@@ -1,40 +1,12 @@
 import React, { useEffect, useState, useRef } from 'react';
 import ReactDOM from 'react-dom';
-import Tree from './treeView';
-import Atoms from './atomView';
-import Navbar from './Components/Navbar'
+
+import Container from './Container'
 
 const App = () => {
   return (
     <div>
       <Container />
-    </div>
-  )
-}
-
-const port = chrome.runtime.connect({ name: 'test' })
-
-const Container = () => {
-
-  const [tree, setTree] = useState();
-
-  useEffect(() => {
-    port.postMessage({
-      name: 'connect',
-      tabID: chrome.devtools.inspectedWindow.tabId,
-    })
-
-    port.onMessage.addListener((message) => {
-      if (message.length === 3) {
-        setTree(message)
-      }
-    })
-  }, [])
-
-  return (
-    <div id='main-container'>
-      <Tree tree={tree} />
-      <Navbar tree={tree} />
     </div>
   )
 }
